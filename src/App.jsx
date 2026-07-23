52d1c1f1477d2b5ed7f6b78285d9b5882e3b5d7f
pmbfsa <paulomarcioba@gmail.com>
2026-07-23 17:05:23 -0300

    Create Directory component Move categories style to Directory style Remove category style empty file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import CategoryItem from './components/category-item/category-item.component.jsx';
-import './categories.style.scss';
+import Directory from './components/directory/directory.component.jsx';
 
 const App = () => {
   const categories = [
@@ -30,13 +29,7 @@ const App = () => {
     },
   ];
 
-  return (
-    <div className="categories-container">
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
-  );
+  return <Directory categories={categories} />;
 };
 
 export default App;
