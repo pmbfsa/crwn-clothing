bfcbaa58e11197fa29facf02b28fbf616212c258
pmbfsa <paulomarcioba@gmail.com>
2026-08-12 17:43:41 -0300

    Add base path in vite config file

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/crwn-clothing/',
   build: {
     outDir: 'docs',
     emptyOutDir: true,
